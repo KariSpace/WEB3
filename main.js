9538dc8a26413be45184f4e3d1e41c41587b5cce
KariSpace <kpivtorak55@gmail.com>
2019-11-28 22:37:55 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function openTab(evt, tab) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("nav-item");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" acti", "");
+    }
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " acti";
+}
